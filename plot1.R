805d8c6e91a88dc8df5d425b0681549445f24eb8
Mohsen Nikfarjam <mohsennikfarjam@yahoo.com>
2026-01-10 22:20:43 -0600

    Add plot1.R

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# Download and unzip the dataset if it doesn't exist
+dataset_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (!file.exists("household_power_consumption.txt")) {
+    download.file(dataset_url, "dataset.zip")
+    unzip("dataset.zip")
+}
+
+# Read and subset data
+full_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+data <- subset(full_data, Date %in% c("1/2/2007", "2/2/2007"))
+
+# Process plot 1
+data$Global_active_power <- as.numeric(data$Global_active_power)
+png("plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
